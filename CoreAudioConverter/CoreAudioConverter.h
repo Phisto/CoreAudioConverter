d898bb24120d4154520d2242ade0f545ea896a6a
phisto05 <phisto05@gmail.com>
2015-09-21 02:57:09 +0200

    Initial Commit

---
+++ CoreAudioConverter/CoreAudioConverter.h
@@ -0,0 +1,19 @@
+//
+//  CoreAudioConverter.h
+//  CoreAudioConverter
+//
+//  Created by Simon Gaus on 21.09.15.
+//  Copyright © 2015 Simon Gaus. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CoreAudioConverter.
+FOUNDATION_EXPORT double CoreAudioConverterVersionNumber;
+
+//! Project version string for CoreAudioConverter.
+FOUNDATION_EXPORT const unsigned char CoreAudioConverterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreAudioConverter/PublicHeader.h>
+
+
