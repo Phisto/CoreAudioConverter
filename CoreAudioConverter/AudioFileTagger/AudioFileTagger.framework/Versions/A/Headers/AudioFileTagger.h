905b75abd20223dc0347d482ebdf8bb65d21bd3f
phisto05 <phisto05@gmail.com>
2015-10-01 13:16:35 +0200

    void

---
+++ CoreAudioConverter/AudioFileTagger/AudioFileTagger.framework/Versions/A/Headers/AudioFileTagger.h
@@ -0,0 +1,19 @@
+//
+//  AudioFileTagger.h
+//  AudioFileTagger
+//
+//  Created by Simon Gaus on 01.10.15.
+//  Copyright © 2015 Simon Gaus. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AudioFileTagger.
+FOUNDATION_EXPORT double AudioFileTaggerVersionNumber;
+
+//! Project version string for AudioFileTagger.
+FOUNDATION_EXPORT const unsigned char AudioFileTaggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioFileTagger/PublicHeader.h>
+//#import "MP3Tagger.h"
+#import <AudioFileTagger/MP3Tagger.h>
